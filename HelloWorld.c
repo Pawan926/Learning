6a34efe04edaee2f5cc0f530f69f4abffb448155
unknown <pawansharma902484@gmail.com>
2020-08-31 20:54:48 +0530

    Learning1

---
+++ HelloWorld.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    printf("Insert Two Numbers\n");
+    scanf("%d %d",&a,&b);
+    printf(" The Sum is %d \n The Subtraction is %d \n The Multiplication is %d \n The Division is %d \n The Remainder is %d",a+b,a-b,a*b,a/b,a%b);
+}
